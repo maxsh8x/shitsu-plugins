9ad4a3d12940f0ab04b655ee9bfc0a0d8c8a449b
Max Sh <neko@protonmail.ch>
2016-03-05 16:17:47 +0300

    Added files via upload

---
+++ youtube.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urlparse
+from shitsu import xmpp
+from shitsu import modules
+from base64 import b64encode
+from urllib2 import urlopen
+
+
+class Youtube(modules.MessageModule):
+    args = (1,)
+    highlight = False
+
+    def run(self, url):
+        """
+        Youtube preview.
+        For Psi+ (webkit version)  https://sourceforge.net/projects/psiplus/files/:
+        Options -> Plugins -> Extended Options Plugin -> Groupchat -> Enable HTML rendering
+        """
+        url_data = urlparse.urlparse(url)
+        query = urlparse.parse_qs(url_data.query)
+        video = query["v"][0]
+        data = urlopen("http://img.youtube.com/vi/%s/mqdefault.jpg" % video)
+        encoded_data = b64encode(data.read())
+        img = xmpp.Node("img", attrs={"alt": "img", "src": "data:image/jpeg;base64," + encoded_data})
+        return "Enable HTML rendering to see this preview", " <br />" + unicode(img)
